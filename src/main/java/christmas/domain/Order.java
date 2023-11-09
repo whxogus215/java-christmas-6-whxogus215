9921e4781eae40619a222da5570b67ec8de90988
whxogus215 <whxogus215@naver.com>
2023-11-10 08:03:23 +0900

    feat(Order): 메뉴 중복 검사 메서드 구현

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -4,7 +4,10 @@ import christmas.domain.menu.Menu;
 import christmas.domain.menu.MenuType;
 import christmas.enums.ErrorMessage;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Order {
     private Map<MenuType, Integer> orders = new HashMap<>();
@@ -15,12 +18,25 @@ public class Order {
     }
 
     private void validateMenuType(String[] menuTypes) {
+        validateIsInMenu(menuTypes);
+        validateDuplicateInMenu(menuTypes);
+    }
+
+    private void validateIsInMenu(String[] menuTypes) {
         for (String menuType : menuTypes) {
             if (Menu.isNotInMenu(menuType)) {
                 throw new IllegalArgumentException(
                         ErrorMessage.ERROR_CODE.getMessage()
-                            + ErrorMessage.NOT_IN_MENU.getMessage());
+                                + ErrorMessage.NOT_IN_MENU.getMessage());
             }
+        }
+    }
+
+    private void validateDuplicateInMenu(String[] menuTypes) {
+        Set<String> menus = new HashSet<>(List.of(menuTypes));
+        if (menus.size() != menuTypes.length) {
+            throw new IllegalArgumentException(
+                    ErrorMessage.ERROR_CODE.getMessage() + ErrorMessage.NOT_IN_MENU.getMessage());
         }
     }
 
