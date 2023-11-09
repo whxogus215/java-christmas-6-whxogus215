b934fc904de95850ea4254c52976f863c4e11bc5
whxogus215 <whxogus215@naver.com>
2023-11-10 00:03:41 +0900

    feat(InputView): 입력 값이 1과 31 사이에 있는지 검증 메서드 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -5,17 +5,36 @@ import christmas.enums.ErrorMessage;
 
 public class InputView {
     private final String error = "[ERROR]";
+
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String input = Console.readLine();
-        // 방문 날짜가 숫자 형태인지 검증
-        validateInputNumber(input);
-        // 방문 날짜(input)이 1이상 31이하인지 검증
+        int day;
+        while (true) {
+            String input = Console.readLine();
+            try {
+                // 방문 날짜가 숫자 형태인지 검증
+                validateInputNumber(input);
+                // 방문 날짜(input)이 1이상 31이하인지 검증
+                day = validateDayInRange(input);
+                break;
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
+        return day;
     }
 
     private void validateInputNumber(String input) {
         if (!input.matches("^-?\\d+$")) {
             throw new IllegalArgumentException(error + ErrorMessage.NOT_NUMBER.getMessage());
         }
+    }
+
+    private int validateDayInRange(String input) {
+        int day = Integer.parseInt(input);
+        if (day < 1 || day > 31) {
+            throw new IllegalArgumentException(error + ErrorMessage.DAY_NOT_INRANGE.getMessage());
+        }
+        return day;
     }
 }
