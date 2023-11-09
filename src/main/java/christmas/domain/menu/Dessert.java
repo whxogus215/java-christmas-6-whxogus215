6c0857fbd7840b6cc6d2a76538179688d8c7edaa
whxogus215 <whxogus215@naver.com>
2023-11-10 01:10:50 +0900

    feat: 디저트 Enum 객체 구현

---
+++ src/main/java/christmas/domain/menu/Dessert.java
@@ -0,0 +1,22 @@
+package christmas.domain.menu;
+
+public enum Dessert {
+    CAKE("초코케이크", 15000),
+    ICECREAM("아이스크림", 5000);
+
+    private final String name;
+    private final int price;
+
+    Dessert(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
