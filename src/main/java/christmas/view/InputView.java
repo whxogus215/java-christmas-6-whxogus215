983b580a1fe2150286df0b5ff11c4d6194e4f901
whxogus215 <whxogus215@naver.com>
2023-11-10 02:14:50 +0900

    refactor(InputView): ERROR CODE Enum 객체 값으로 변경

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import christmas.enums.ErrorMessage;
 
 public class InputView {
-    private final String error = "[ERROR] ";
-
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         int day;
@@ -23,7 +21,8 @@ public class InputView {
     }
 
     public String[] readMenuAndCount() {
-        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        System.out.println(
+                "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input;
         while (true) {
             input = Console.readLine();
@@ -39,21 +38,24 @@ public class InputView {
 
     private void validateInputNumber(String input) {
         if (!input.matches("^-?\\d+$")) {
-            throw new IllegalArgumentException(error + ErrorMessage.NOT_NUMBER.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.ERROR_CODE.getMessage()
+                    + ErrorMessage.NOT_NUMBER.getMessage());
         }
     }
 
     private int validateDayInRange(String input) {
         int day = Integer.parseInt(input);
         if (day < 1 || day > 31) {
-            throw new IllegalArgumentException(error + ErrorMessage.DAY_NOT_INRANGE.getMessage());
+            throw new IllegalArgumentException(ErrorMessage.ERROR_CODE.getMessage()
+                    + ErrorMessage.DAY_NOT_INRANGE.getMessage());
         }
         return day;
     }
 
     private void validateInputBlank(String input) {
         if (input.isBlank()) {
-            throw new IllegalArgumentException(error + ErrorMessage.NOT_IN_MENU.getMessage());
+            throw new IllegalArgumentException(
+                    ErrorMessage.ERROR_CODE.getMessage() + ErrorMessage.NOT_IN_MENU.getMessage());
         }
     }
 
