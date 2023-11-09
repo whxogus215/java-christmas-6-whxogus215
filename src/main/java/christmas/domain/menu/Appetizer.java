550f8584354b19ab7cb884fba24fd8c433f72309
whxogus215 <whxogus215@naver.com>
2023-11-10 01:07:04 +0900

    feat: 애피타이저 Enum 객체 구현

---
+++ src/main/java/christmas/domain/menu/Appetizer.java
@@ -0,0 +1,23 @@
+package christmas.domain.menu;
+
+public enum Appetizer {
+    MUSHROOM_SOUP("양송이수프", 6000),
+    TAPAS("타파스", 5500),
+    SALAD("시저샐러드", 8000);
+
+    private final String name;
+    private final int price;
+
+    Appetizer(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
