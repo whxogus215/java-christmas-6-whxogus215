7d0b19f8dbbfa4bf608ba224a36aa06af0a68d73
whxogus215 <whxogus215@naver.com>
2023-11-10 08:26:47 +0900

    feat(ErrorMessage): 메뉴 최대 20개 에러 메시지 추가

--- src/main/java/christmas/enums/ErrorMessage.java
+++ src/main/java/christmas/enums/ErrorMessage.java
@@ -5,7 +5,8 @@ public enum ErrorMessage {
     NOT_NUMBER("숫자를 입력해주세요."),
     DAY_NOT_INRANGE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
     NOT_IN_MENU("유효하지 않은 주문입니다. 다시 입력해 주세요."),
-    NOT_ALL_DRINK("음료만 주문할 수 없습니다. 다시 주문해주세요.");
+    NOT_ALL_DRINK("음료만 주문할 수 없습니다. 다시 주문해주세요."),
+    MAX_ORDER("메뉴는 최대 20개까지만 주문할 수 있습니다. 다시 주문해주세요.");
 
     private final String message;
 
