412941cee68eb20b720f30db1649fa07bf2e54c1
whxogus215 <whxogus215@naver.com>
2023-11-10 11:14:29 +0900

    refactor(DiscountType): 함수형 인터페이스 적용

--- src/main/java/christmas/domain/discount/DiscountType.java
+++ src/main/java/christmas/domain/discount/DiscountType.java
@@ -3,37 +3,38 @@ package christmas.domain.discount;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 public enum DiscountType {
     /**
-     * 1. 크리스마스 디데이 할인 (1~25)
-     * 2. 평일 할인 (3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31)
-     * 3. 주말 할인 (1,2,8,9,15,16,22,23,29,30)
-     * 4. 특별 할인 (3,10,17,24,25,31)
-     * 날짜는 리스트로 관리
-     * 각 입력 값에 해당하는 경우 할인 이름과 할인 금액을 반환
+     * 1. 크리스마스 디데이 할인 (1~25) 2. 평일 할인 (3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31)
+     * 3. 주말 할인 (1,2,8,9,15,16,22,23,29,30) 4. 특별 할인 (3,10,17,24,25,31) 날짜는 리스트로 관리 각 입력 값에 해당하는 경우
+     * 할인 이름과 할인 금액을 반환
      */
     CHRISTMAS(
             Arrays.asList(
                     1, 2, 3, 4, 5, 6, 7, 8, 9,
                     10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                    20, 21, 22, 23, 24, 25)),
+                    20, 21, 22, 23, 24, 25),
+            day -> DiscountPrice.DEFAULT.getPrice() + (day - 1) * DiscountPrice.UNIT.getPrice()),
     WEEKDAY(Arrays.asList(
             3, 4, 5, 6, 7,
             10, 11, 12, 13, 14,
             17, 18, 19, 20, 21,
             24, 25, 26, 27, 28,
-            31)),
+            31), day -> DiscountPrice.YEAR.getPrice()),
     WEEKEND(Arrays.asList(
             1, 2, 8, 9, 15,
-            16, 22, 23, 29, 30)),
-    SPECIAL(Arrays.asList(3, 10, 17, 24, 25, 31)),
-    NONE(null);
+            16, 22, 23, 29, 30), day -> DiscountPrice.YEAR.getPrice()),
+    SPECIAL(Arrays.asList(3, 10, 17, 24, 25, 31), day -> DiscountPrice.DEFAULT.getPrice()),
+    NONE(null, day -> DiscountPrice.ZERO.getPrice());
 
     private final List<Integer> discountDays;
+    private final Function<Integer, Integer> discountFunction;
 
-    DiscountType(List<Integer> discountDays) {
+    DiscountType(List<Integer> discountDays, Function<Integer, Integer> discountFunction) {
         this.discountDays = discountDays;
+        this.discountFunction = discountFunction;
     }
 
     public static List<DiscountType> findDiscountTypeByDay(int day) {
@@ -48,16 +49,7 @@ public enum DiscountType {
         return types;
     }
 
-    public static int getDiscountPrice(DiscountType type, int day) {
-        if (type.equals(CHRISTMAS)) {
-            return DiscountPrice.DEFAULT.getPrice() + (day - 1) * DiscountPrice.UNIT.getPrice();
-        }
-        if (type.equals(WEEKDAY) || type.equals(WEEKEND)) {
-            return DiscountPrice.YEAR.getPrice();
-        }
-        if (type.equals(SPECIAL)) {
-            return DiscountPrice.DEFAULT.getPrice();
-        }
-        return 0;
+    public int getDiscountPrice(int day) {
+        return discountFunction.apply(day);
     }
 }
