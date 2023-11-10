4a8534aed28705407dc79b73cd69095a30ddc3d5
whxogus215 <whxogus215@naver.com>
2023-11-10 17:59:45 +0900

    refactor(InputView): InputView가 날짜만 입력 받도록 리팩토링
    readDate()는 해당 숫자 값이 1부터 31 사이의 숫자인지만 검증함
    예외가 발생할 경우, Controller에서 Catch하여 다시 입력 요청

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,23 +1,18 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.Regex;
 import christmas.message.ErrorMessage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        int date;
-        while (true) {
-            String input = Console.readLine();
-            try {
-                validateInputNumber(input);
-                date = validateDayInRange(input);
-                break;
-            } catch (IllegalArgumentException exception) {
-                System.out.println(exception.getMessage());
-            }
-        }
-        return date;
+        String input = Console.readLine();
+        validateInputNumber(input);
+        validateDateInRange(input);
+        return Integer.parseInt(input);
     }
 
     public String[] readMenuAndCount() {
