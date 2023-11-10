e86225ff9bf9841f226eb40e12b5a4d6094a39cc
whxogus215 <whxogus215@naver.com>
2023-11-10 15:52:15 +0900

    test(InputView): ,를 기준으로 구분했을 때 공백 검증 메서드 테스트

---
+++ src/test/java/christmas/exception/InputExceptionTest.java
@@ -0,0 +1,24 @@
+package christmas.exception;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.Application;
+import christmas.message.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+public class InputExceptionTest extends NsTest {
+    @Test
+    void 주문_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("3", "제로콜라-3, ");
+            assertThat(output()).contains(ErrorMessage.NOT_IN_MENU.getMessage());
+        });
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
