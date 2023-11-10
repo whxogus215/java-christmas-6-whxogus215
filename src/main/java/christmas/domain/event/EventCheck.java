b525b4bc497577b92ff90293743752914370e9aa
whxogus215 <whxogus215@naver.com>
2023-11-10 12:16:07 +0900

    feat(EventCheck): 할인 전 총주문 금액을 바탕으로 증정 목록 반환 구현

--- src/main/java/christmas/domain/event/EventCheck.java
+++ src/main/java/christmas/domain/event/EventCheck.java
@@ -1,12 +1,22 @@
 package christmas.domain.event;
 
-public class EventCheck {
-    public void checkDiscount(int totalPrice) {
+import christmas.domain.discount.DiscountType;
+import christmas.domain.menu.Menu;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class EventCheck {
+    private final int GIFT_MIN_PRICE = 120000;
+
+    public List<DiscountType> checkDiscountType(int day) {
+        return DiscountType.findDiscountTypeByDay(day);
     }
 
-    public void checkGift(int totalPrice) {
-
+    public Map<Menu, Integer> checkGift(int totalPrice) {
+        Map<Menu, Integer> gifts = new HashMap<>();
+        gifts.put(Menu.CHAMPAGNE, totalPrice % GIFT_MIN_PRICE);
+        return gifts;
     }
 
     public void checkBadge(int totalDiscountPrice, int giftPrice) {
