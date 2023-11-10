dd7fd986249cd69cd8393267adf73ed3520e5ea7
whxogus215 <whxogus215@naver.com>
2023-11-10 23:41:23 +0900

    feat(OutputView): 12월 이벤트 배지 출력 메서드 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -50,4 +50,9 @@ public class OutputView {
         System.out.println("<할인 후 예상 결제 금액>");
         System.out.println(price + "원");
     }
+
+    public void printEventBadge(String badge) {
+        System.out.println("<12월 이벤트 배지>");
+        System.out.println(badge);
+    }
 }
