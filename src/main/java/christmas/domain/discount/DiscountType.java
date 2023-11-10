547690f7b6a3dfa1ad9cf9634dc3aa2016b7976b
whxogus215 <whxogus215@naver.com>
2023-11-10 11:18:03 +0900

    feat(DiscountType): 할인 이름 추가

--- src/main/java/christmas/domain/discount/DiscountType.java
+++ src/main/java/christmas/domain/discount/DiscountType.java
@@ -6,33 +6,33 @@ import java.util.List;
 import java.util.function.Function;
 
 public enum DiscountType {
-    /**
-     * 1. 크리스마스 디데이 할인 (1~25) 2. 평일 할인 (3,4,5,6,7,10,11,12,13,14,17,18,19,20,21,24,25,26,27,28,31)
-     * 3. 주말 할인 (1,2,8,9,15,16,22,23,29,30) 4. 특별 할인 (3,10,17,24,25,31) 날짜는 리스트로 관리 각 입력 값에 해당하는 경우
-     * 할인 이름과 할인 금액을 반환
-     */
     CHRISTMAS(
-            Arrays.asList(
+            "크리스마스 디데이 할인", Arrays.asList(
                     1, 2, 3, 4, 5, 6, 7, 8, 9,
                     10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
                     20, 21, 22, 23, 24, 25),
             day -> DiscountPrice.DEFAULT.getPrice() + (day - 1) * DiscountPrice.UNIT.getPrice()),
-    WEEKDAY(Arrays.asList(
+    WEEKDAY("평일 할인", Arrays.asList(
             3, 4, 5, 6, 7,
             10, 11, 12, 13, 14,
             17, 18, 19, 20, 21,
             24, 25, 26, 27, 28,
             31), day -> DiscountPrice.YEAR.getPrice()),
-    WEEKEND(Arrays.asList(
+    WEEKEND("주말 할인", Arrays.asList(
             1, 2, 8, 9, 15,
             16, 22, 23, 29, 30), day -> DiscountPrice.YEAR.getPrice()),
-    SPECIAL(Arrays.asList(3, 10, 17, 24, 25, 31), day -> DiscountPrice.DEFAULT.getPrice()),
-    NONE(null, day -> DiscountPrice.ZERO.getPrice());
+    SPECIAL("특별 할인", Arrays.asList(3, 10, 17, 24, 25, 31),
+            day -> DiscountPrice.DEFAULT.getPrice()),
+    NONE("없음", null, day -> DiscountPrice.ZERO.getPrice());
 
+    private final String discountName;
     private final List<Integer> discountDays;
     private final Function<Integer, Integer> discountFunction;
 
-    DiscountType(List<Integer> discountDays, Function<Integer, Integer> discountFunction) {
+    DiscountType(String discountName,
+                 List<Integer> discountDays,
+                 Function<Integer, Integer> discountFunction) {
+        this.discountName = discountName;
         this.discountDays = discountDays;
         this.discountFunction = discountFunction;
     }
@@ -51,5 +51,9 @@ public enum DiscountType {
 
     public int getDiscountPrice(int day) {
         return discountFunction.apply(day);
+    }
+
+    public String getDiscountName() {
+        return discountName;
     }
 }
