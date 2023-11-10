682563af9c460cfbecb4f04362c43967d19e9086
whxogus215 <whxogus215@naver.com>
2023-11-10 20:04:39 +0900

    feat: Converter 객체 구현

---
+++ src/main/java/christmas/Converter.java
@@ -0,0 +1,54 @@
+package christmas;
+
+import christmas.message.ErrorMessage;
+import christmas.view.InputView;
+
+public class Converter {
+    public static String[] getMenuNames(String input) {
+        String[] orders = splitOrderByComma(input);
+        return convertMenusByHypen(orders);
+    }
+
+    public static int[] getQuantities(String input) {
+        String[] orders = splitOrderByComma(input);
+        return convertCountByHypen(orders);
+    }
+
+    private static String[] splitOrderByComma(String input) {
+        String[] splitOrders = input.split(",");
+        for (String splitedOrder : splitOrders) {
+            InputView.validateInputBlank(splitedOrder);
+        }
+        return splitOrders;
+    }
+
+    private static String[] convertMenusByHypen(String[] orders) {
+        String[] menus = new String[orders.length];
+        for (int i = 0; i < orders.length; i++) {
+            int index = orders[i].lastIndexOf("-");
+            String menu = orders[i].substring(0, index);
+            if (!menu.matches(Regex.ONLY_STRING.getRegex())) {
+                throw new IllegalArgumentException(
+                        ErrorMessage.ERROR_CODE.getMessage()
+                                + ErrorMessage.NOT_IN_MENU.getMessage());
+            }
+            menus[i] = menu;
+        }
+        return menus;
+    }
+
+    private static int[] convertCountByHypen(String[] orders) {
+        int[] quantities = new int[orders.length];
+        for (int i = 0; i < orders.length; i++) {
+            int index = orders[i].lastIndexOf("-");
+            String count = orders[i].substring(index + 1);
+            if (!count.matches(Regex.NOT_EMPTY_NUMBER.getRegex())) {
+                throw new IllegalArgumentException(
+                        ErrorMessage.ERROR_CODE.getMessage()
+                                + ErrorMessage.NOT_IN_MENU.getMessage());
+            }
+            quantities[i] = Integer.parseInt(count);
+        }
+        return quantities;
+    }
+}
