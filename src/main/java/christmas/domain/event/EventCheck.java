da8013648737e2ec37074e0cd20b8ae17a27afc0
whxogus215 <whxogus215@naver.com>
2023-11-10 09:46:34 +0900

    feat: 이벤트 체크 객체의 인터페이스 구현

---
+++ src/main/java/christmas/domain/event/EventCheck.java
@@ -0,0 +1,15 @@
+package christmas.domain.event;
+
+public class EventCheck {
+    public void checkDiscount(int totalPrice) {
+
+    }
+
+    public void checkGift(int totalPrice) {
+
+    }
+
+    public void checkBadge(int totalDiscountPrice, int giftPrice) {
+
+    }
+}
