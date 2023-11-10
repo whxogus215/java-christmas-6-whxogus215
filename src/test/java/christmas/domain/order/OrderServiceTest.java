c7f7dca4034a30f9b30e825172e2607a0a82e2f9
whxogus215 <whxogus215@naver.com>
2023-11-10 09:40:17 +0900

    test(OrderService): 주문 서비스의 할인 전 총주문 금액 테스트

---
+++ src/test/java/christmas/domain/order/OrderServiceTest.java
@@ -0,0 +1,41 @@
+package christmas.domain.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import christmas.domain.Order;
+import christmas.domain.menu.Menu;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class OrderServiceTest {
+    OrderService service;
+
+    @BeforeEach
+    void before() {
+        service = new OrderService();
+    }
+
+    @DisplayName("주문 서비스에서 할인 전 총주문 금액 확인")
+    @ParameterizedTest
+    @CsvSource({
+            "양송이수프, 티본스테이크, 초코케이크, 제로콜라, 12, 2, 3, 1",
+            "타파스, 바비큐립, 아이스크림, 샴페인, 1, 2, 13, 2"
+    })
+    void orderServiceTotalPriceTest(String menu1, String menu2, String menu3, String menu4,
+                                    int quantity1, int quantity2, int quantity3, int quantity4) {
+        String[] menuTypes = {menu1, menu2, menu3, menu4};
+        int[] quantities = {quantity1, quantity2, quantity3, quantity4};
+        Order order = new Order(menuTypes, quantities);
+
+        int orderPrice = 0;
+        for (String menu : order.getOrders().keySet()) {
+            Menu findMenu = Menu.findMenuByName(menu);
+            orderPrice += (order.getOrders().get(menu) * findMenu.getPrice());
+        }
+        service.order(menuTypes, quantities);
+
+        assertThat(orderPrice).isEqualTo(service.getTotalOrderPrice());
+    }
+}
