ea5dd06fb6421a89781017b33dfb8c059ae82490
whxogus215 <whxogus215@naver.com>
2023-11-11 08:46:55 +0900

    refactor(EventBadge): 총혜택 금액 변수 이름 변경

--- src/main/java/christmas/domain/event/EventBadge.java
+++ src/main/java/christmas/domain/event/EventBadge.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 import java.util.function.Function;
 
 public enum EventBadge {
-    NONE("없음", totalBenefit -> totalBenefit < 5000),
-    STAR("별", totalBenefit -> totalBenefit >= 5000 && totalBenefit < 10000),
-    TREE("트리", totalBenefit -> totalBenefit >= 10000 && totalBenefit < 20000),
-    SANTA("산타", totalBenefit -> totalBenefit >= 20000);
+    NONE("없음", totalBenefitAmount -> totalBenefitAmount < 5000),
+    STAR("별", totalBenefitAmount -> totalBenefitAmount >= 5000 && totalBenefitAmount < 10000),
+    TREE("트리", totalBenefitAmount -> totalBenefitAmount >= 10000 && totalBenefitAmount < 20000),
+    SANTA("산타", totalBenefitAmount -> totalBenefitAmount >= 20000);
 
     private final String name;
     private final Function<Integer, Boolean> function;
@@ -17,15 +17,15 @@ public enum EventBadge {
         this.function = function;
     }
 
-    public static EventBadge getEventBadge(int totalBenefit) {
+    public static EventBadge getEventBadge(int totalBenefitAmount) {
         return Arrays.stream(values())
-                .filter(badge -> badge.isEventBadge(totalBenefit))
+                .filter(badge -> badge.isEventBadge(totalBenefitAmount))
                 .findFirst()
                 .orElse(NONE);
     }
 
-    private boolean isEventBadge(int totalBenefit) {
-        return function.apply(totalBenefit);
+    private boolean isEventBadge(int totalBenefitAmount) {
+        return function.apply(totalBenefitAmount);
     }
 
     public String getName() {
