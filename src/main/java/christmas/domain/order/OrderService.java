4241c38c481ea4a30ce432acac88eb399c5ee1ed
whxogus215 <whxogus215@naver.com>
2023-11-10 14:43:01 +0900

    feat(OrderService): 적용된 할인 타입 반환 메서드 구현

--- src/main/java/christmas/domain/order/OrderService.java
+++ src/main/java/christmas/domain/order/OrderService.java
@@ -1,12 +1,20 @@
 package christmas.domain.order;
 
 import christmas.domain.Order;
+import christmas.domain.discount.DiscountType;
+import christmas.domain.event.EventCheck;
 import christmas.domain.menu.Menu;
+import java.util.List;
 import java.util.Map;
 
 public class OrderService {
+    private final EventCheck eventCheck;
     private int totalOrderPrice;
     private int totalDiscountPrice;
+
+    public OrderService() {
+        eventCheck = new EventCheck();
+    }
 
     public void order(String[] menuTypes, int[] quantities) {
         Order order = createOrder(menuTypes, quantities);
@@ -15,6 +23,14 @@ public class OrderService {
             Menu findMenu = Menu.findMenuByName(menu);
             this.totalOrderPrice += (orders.get(menu) * findMenu.getPrice());
         }
+    }
+
+    public List<DiscountType> getDiscountTypes(int day) {
+        List<DiscountType> discountTypes = eventCheck.checkDiscountType(day);
+        for (DiscountType discountType : discountTypes) {
+            totalDiscountPrice += discountType.getDiscountPrice(day);
+        }
+        return discountTypes;
     }
 
     private Order createOrder(String[] menuTypes, int[] quantities) {
