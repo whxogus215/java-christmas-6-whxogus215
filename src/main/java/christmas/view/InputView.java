49d0148373ff5af6b90b0ea11c630098e33ff942
whxogus215 <whxogus215@naver.com>
2023-11-11 11:05:49 +0900

    feat(InputView): 주문 내용의 공백 제거 기능 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -18,6 +18,7 @@ public class InputView {
                 "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
         String input = Console.readLine();
         validateInputBlank(input);
+        input = input.trim();
         return input;
     }
 
