e017c190c62ef27557c3232c5906ebde0632a584
whxogus215 <whxogus215@naver.com>
2023-11-14 09:57:26 +0900

    refactor(Test): Order에 있는 검증 메서드를 Controller로 옮긴 후 테스트 메서드 작성

--- src/test/java/christmas/domain/order/OrderTest.java
+++ src/test/java/christmas/domain/order/OrderTest.java
@@ -1,83 +1,14 @@
 package christmas.domain.order;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import christmas.domain.menu.MenuType;
-import christmas.domain.order.Order;
-import christmas.utils.ErrorMessage;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class OrderTest {
-    @DisplayName("메뉴판에 없는 메뉴있을 경우, 예외 발생")
-    @Test
-    void orderValidateNotInMenuTest() {
-        String[] menuTypes = {"메로나", "등갈비", "스프라이트"};
-        int[] quantities = {1, 2, 3};
-
-        assertThatThrownBy(() -> new Order(menuTypes, quantities))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.NOT_IN_MENU.getMessage());
-    }
-
-    @DisplayName("메뉴판에 있는 메뉴있을 경우, 예외 발생X")
-    @Test
-    void orderValidateInMenuTest() {
-        String[] menuTypes = {"양송이수프", "티본스테이크", "초코케이크", "제로콜라"};
-        int[] quantities = {1, 2, 3, 4};
-
-        assertThatCode(() -> new Order(menuTypes, quantities))
-                .doesNotThrowAnyException();
-    }
-
-    @DisplayName("메뉴 개수가 1 미만일 때, 예외 발생")
-    @Test
-    void orderValidateOneLessQuantityTest() {
-        String[] menuTypes = {"양송이수프", "티본스테이크", "초코케이크", "제로콜라"};
-        int[] quantities = {0, 2, 3, 4};
-
-        assertThatThrownBy(() -> new Order(menuTypes, quantities))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.NOT_IN_MENU.getMessage());
-    }
-
-    @DisplayName("메뉴 개수가 1 이상일 때, 예외 발생X")
-    @Test
-    void orderValidateOneMoreQuantityTest() {
-        String[] menuTypes = {"양송이수프", "티본스테이크", "초코케이크", "제로콜라"};
-        int[] quantities = {1, 2, 3, 4};
-
-        assertThatCode(() -> new Order(menuTypes, quantities))
-                .doesNotThrowAnyException();
-    }
-
-    @DisplayName("메뉴 개수가 중복일 때, 예외 발생")
-    @Test
-    void orderValidateDuplicatedMenuTest() {
-        String[] menuTypes = {"양송이수프", "양송이수프", "초코케이크", "제로콜라"};
-        int[] quantities = {1, 2, 3, 4};
-
-        assertThatThrownBy(() -> new Order(menuTypes, quantities))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.NOT_IN_MENU.getMessage());
-    }
-
-    @DisplayName("메뉴가 20개 이상일 때, 예외 발생")
-    @Test
-    void orderValidateMaxMenuTest() {
-        String[] menuTypes = {"양송이수프", "티본스테이크", "초코케이크", "제로콜라"};
-        int[] quantities = {12, 2, 3, 10};
-
-        assertThatThrownBy(() -> new Order(menuTypes, quantities))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(ErrorMessage.MAX_ORDER.getMessage());
-    }
-
     @DisplayName("메뉴가 정상적으로 저장되어 있는지 확인")
     @ParameterizedTest
     @CsvSource({
